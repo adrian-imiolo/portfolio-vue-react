8e7b6e99b7b569d2e9560d56d5d22a97249e8b8b
Adrian Imioło <adrian.imiolo11@gmail.com>
2023-09-04 12:03:21 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/adrian-imiolo/portfolio"
+                href="https://github.com/adrian-imiolo/portfolio-vue-react"
                 target="_blank"
                 className="fork-btn-inner"
               >
